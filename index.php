6cfa2a12d378e43844cbaa8c708cdc1a0e10ec7a
Denniz03 <Danny.korthout@gmail.com>
2023-08-27 22:54:03 +0200

    Update index.php
    - meta theme and mobile app options

--- index.php
+++ index.php
@@ -43,6 +43,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="<?php echo $userColor ?>">
+    <meta name="msapplication-navbutton-color" content="<?php echo $userColor ?>">
+    <meta name="apple-mobile-web-app-status-bar-style" content="<?php echo $userColor ?>">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>Moppen Pagina</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
